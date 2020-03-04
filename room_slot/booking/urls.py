48f5ad35cec84d312722947140d9e148a3797c19
techievivek <techievivek123@gmail.com>
2020-03-05 02:09:35 +0530

    Completed booking functionality

--- room_slot/booking/urls.py
+++ room_slot/booking/urls.py
@@ -4,6 +4,7 @@ urlpatterns=[
     path('',views.index,name='index'),
     path('book',views.book,name='book'),
     path('contact-us',views.contact,name='contact-us'),
-    path('book-now/<str:id>',views.book_now,name='book-now')
+    path('book-now/<str:id>',views.book_now,name='book-now'),
+    path('confirm-now-booking',views.book_confirm,name="book_confirm")
 
 ]
--- room_slot/booking/views.py
+++ room_slot/booking/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import Contact
-from .models import Rooms
+from .models import Rooms,Booking
+from login.models import Customer
 import datetime
 def index(request):
     return render(request,'booking/index.html',{})
@@ -34,8 +35,10 @@ def book_now(request,id):
             no_of_days=request.session['no_of_days']
             start_date=request.session['start_date']
             end_date=request.session['end_date']
+            request.session['room_no']=id
             data=Rooms.objects.get(room_no=id)
             bill=data.price*int(no_of_days)
+            request.session['bill']=bill
             roomManager=data.manager.username
             return render(request,"booking/book-now.html",{"no_of_days":no_of_days,"room_no":id,"data":data,"bill":bill,"roomManager":roomManager,"start":start_date,"end":end_date})
         else:
@@ -43,6 +46,26 @@ def book_now(request,id):
     else:
         next="book-now/"+id
         return render(request,"login/user_login.html",{"next":next})
+def book_confirm(request):
+    room_no=request.session['room_no']
+    start_date=request.session['start_date']
+    end_date=request.session['end_date']
+    username=request.session['username']
+    user_id=Customer.objects.get(username=username)
+    room=Rooms.objects.get(room_no=room_no)
+    amount=request.session['bill']
+    start_date=datetime.datetime.strptime(start_date, "%d/%b/%Y").date()
+    end_date=datetime.datetime.strptime(end_date, "%d/%b/%Y").date()
+    data=Booking(room_no=room,start_day=start_date,end_day=end_date,amount=amount,user_id=user_id)
+    data.save()
+    del request.session['start_date']
+    del request.session['end_date']
+    del request.session['bill']
+    del request.session['room_no']
+    return render()
+
+            
+
 
 
     
